b1f9d7a3837b82bbea8dd670e70a284085c2254f
EricIsMyHero <ericismyhero2467@gmail.com>
2025-08-15 22:28:02 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', e => {
-    const data = e.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: 'icons/icon-192x192.png' // Bildiriş üçün ikon
-    });
-});
