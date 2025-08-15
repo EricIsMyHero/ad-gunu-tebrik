e31042a42685532b0a2541ba70c934e81ed2e958
EricIsMyHero <ericismyhero2467@gmail.com>
2025-08-15 13:13:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('.')); // frontend fayllar üçün (index.html, style.css, client.js)
+// DƏYİŞİKLİK BURADADIR: Statik fayllar ana qovluqdan ("./") oxunur
+app.use(express.static('.')); 
 
 // MongoDB qoşulması
 mongoose.connect(process.env.MONGO_URI, {
